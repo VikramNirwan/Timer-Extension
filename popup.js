331573063aa54fa4b628e369389cc53811b1eb7e
Vikram Nirwan <vnirwan642@gmail.com>
2023-10-07 12:48:21 +0530

    Implemented Start stop and reset button

--- popup.js
+++ popup.js
@@ -19,3 +19,26 @@ chrome.storage.sync.get(["name"]).then((result) => {
   const name = result.name ?? "?";
   nameElement.textContent = `Your name is: ${name}`;
 });
+
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  });
+});
+
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false,
+  });
+});
